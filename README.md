ce6d5364d99642b3a234cdd2bb05f9ccf6f337ef
Daniel Berman <divibisan@gmail.com>
2017-08-27 14:32:20 -0400

    Uses config.txt file to set NAME/ColorID associations
    Can now use the config.txt file to set any number of NAME/ColorID associations

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Colors calendar events based on criteria
 
 The purpose of this program is to automatically color Google Calendar events by a given criteria, such as the event name, rather than by default calendar color.
+
+The file config.txt holds EventName/ColorID associations.
+Each is on a new line, separate name and color with a comma
--- colorCodeCalendar.py
+++ colorCodeCalendar.py
@@ -5,26 +5,59 @@ from oauth2client.file import Storage
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client import tools
 
+# Define service object as a global variable
 service = ""
 
 
 def main():
     # *** AUTHORIZATION ***
     global service
-    FLOW = flow_from_clientsecrets('client_secrets.json',
+    flow = flow_from_clientsecrets('client_secrets.json',
                                    scope='https://www.googleapis.com/auth/calendar')
-
     storage = Storage('calendar.dat')
     credentials = storage.get()
     if credentials is None or credentials.invalid is True:
-        credentials = tools.run_flow(FLOW, storage)
-
+        credentials = tools.run_flow(flow, storage)
     http = httplib2.Http()
     http = credentials.authorize(http)
-
     service = build(serviceName='calendar', version='v3', http=http)
     # *** END AUTHORIZATION ***
 
+
+    # Read in config file to get name/colorID associations
+    name_color_dict = read_config('config.txt')
+    print(name_color_dict)
+
+    # Choose calendar and get calendar ID
+    #cal_id = choose_calendar()
+    cal_id = '614i74105o8hdn44cqgts47hlc@group.calendar.google.com'
+
+    # Get all events in specific calendar, change color of chosen events
+    page_token = None
+    while True:
+        events = service.events().list(calendarId=cal_id,
+                                       pageToken=page_token).execute()
+        for event in events['items']:
+            summary = event['summary']
+            event_id = event['id']
+            # if matches criteria, change color
+            if summary in name_color_dict:
+                set_event_color(cal_id, event_id, name_color_dict[summary])
+            page_token = events.get('nextPageToken')
+        if not page_token:
+            break
+
+
+def set_event_color(cal_id, event_id, color_id):
+    # Sets the color of the specified event
+    event = service.events().get(calendarId=cal_id, eventId=event_id).execute()
+    event['colorId'] = color_id
+    service.events().update(calendarId=cal_id, eventId=event_id,
+                            body=event).execute()
+
+
+def choose_calendar():
+    # Print list of calendars and prompt user to choose one to work with
     # Print list of calendars
     page_token = None
     while True:
@@ -37,7 +70,6 @@ def main():
         page_token = calendar_list.get('nextPageToken')
         if not page_token:
             break
-
     # Ask user to choose which calendar to work with by number
     #  will keep asking until user inputs a number
     while True:
@@ -47,33 +79,19 @@ def main():
             break
         except ValueError:
             print("Please enter a number")
-
-    # Get the id number of the chosen calendar
-    cal_id = calendar_list['items'][int(choice)]['id']
-
-    # Print list of events in specific calendar
-    page_token = None
-    while True:
-        events = service.events().list(calendarId=cal_id,
-                                       pageToken=page_token).execute()
-        for event in events['items']:
-            summary = event['summary']
-            event_id = event['id']
-            if summary == "DAN":
-                set_event_color(cal_id, event_id, '11')
-            page_token = events.get('nextPageToken')
-        if not page_token:
-            break
+    # Return the id number of the chosen calendar
+    return calendar_list['items'][choice]['id']
 
 
-def set_event_color(cal_id, event_id, color_id):
-    # Sets the color of the specified event
-    global service
-    event_selected = service.events().get(calendarId=cal_id,
-                                          eventId=event_id).execute()
-    event_selected['colorId'] = color_id
-    service.events().update(calendarId=cal_id, eventId=event_id,
-                            body=event_selected).execute()
+def read_config(filename):
+    # Reads in config file and generates a dictionary
+    #  mapping event names to specific colorIDs
+    name_color_dict = {}
+    with open(filename) as config:
+        for line in config:
+            config_record = line.rstrip("\n").split(",")
+            name_color_dict[config_record[0]] = config_record[1]
+    return name_color_dict
 
 if __name__ == "__main__":
     main()
